b2e78232466db2ffba611d2308ddda75e9bc411f
mc20000-01 <ryanskidjay@gmail.com>
2026-02-05 19:44:21 -0800

    rahh dumb box_to_py

--- boxrun/box to py.py
+++ boxrun/box to py.py
@@ -5,6 +5,8 @@ def to_py(code_json):
     for i in code_json:
         cmd = i['cmd']
         args = i['args']
-        
-        if cmd == "say":
-            py = py + "print(" + args[0] + ")\n" + "time.sleep(" + str(args[1]) + ")\n"
+        match cmd:
+            case "say":
+                py = py + "print(" + args[0] + ")\n" + "time.sleep(" + str(args[1]) + ")\n"
+            case "ask":
+                py = py + "input(" + args[0] + ")\n"
