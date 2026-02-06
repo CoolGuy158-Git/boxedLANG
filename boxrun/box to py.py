18f7c5c83b8dbe5fde4f96b738f0a8d93eb891a8
mc20000-01 <ryanskidjay@gmail.com>
2026-02-05 19:35:17 -0800

    add basic say hanleing in box_to_py

--- boxrun/box to py.py
+++ boxrun/box to py.py
@@ -1,8 +1,10 @@
 import box_to_json as b2j
 
-def to_py(code_json)
+def to_py(code_json):
     py = ""
     for i in code_json:
         cmd = i['cmd']
         args = i['args']
+        
         if cmd == "say":
+            py = py + "print(" + args[0] + ")\n" + "time.sleep(" + str(args[1]) + ")\n"
