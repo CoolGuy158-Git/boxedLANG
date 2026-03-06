45fa5e84028bb047f879cd492ed3436283e6193c
mc20000-01 <mc20000scratch@gmail.com>
2026-03-05 20:18:07 -0800

    fixes again :sob:

--- boxrun/main.py
+++ boxrun/main.py
@@ -1,22 +1,19 @@
 from . import box_to_json as bx2json
 from . import box_runner as bxr
-
-
 import argparse
 import pathlib as file
 import os.path
+parser = argparse.ArgumentParser(description="boxedLANG interpreter")
+parser.add_argument("file", help="path to the boxedLANG source file to run")
+args = parser.parse_args()
 
-def main(code):
-    bxr.start_boxed_code(code, args.file)
+def main():
+    CODE = str(file.Path(os.path.expanduser(args.file)).read_text())
+    bxr.start_boxed_code(CODE, args.file)
 
 
 if __name__ == "__main__":
-	import argparse
-	parser = argparse.ArgumentParser(description="boxedLANG interpreter")
-	parser.add_argument("file", help="path to the boxedLANG source file to run")
-	args = parser.parse_args()
-    CODE = str(file.Path(os.path.expanduser(args.file)).resolve().read_text())
 	try:
-		main(CODE)
+		main()
 	except KeyboardInterrupt:
 		print("KeyboardInterrupt by user")
